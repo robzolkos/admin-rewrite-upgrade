70e4cb5ca5e575619d9956c61043aeef055721f9
Rob Zolkos <rob@zolkos.com>
2020-03-30 12:33:31 -0400

    Move files from old project over

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # This is where the home page lives
+  map.root :controller => 'auth', :action => 'index'
+
+#  map.login  '/',  :controller => 'auth', :action => 'login'
+  map.logout '/logout', :controller => 'auth', :action => 'logout'
+
+  map.resources :domains, :active_scaffold => true do |domain|
+    domain.resources :users, :active_scaffold => true
+    domain.resources :forwardings, :active_scaffold => true
+  end
+
+  # Allow downloading Web Service WSDL as a file with an extension
+  # instead of a file named 'wsdl'
+  map.connect ':controller/service.wsdl', :action => 'wsdl'
+
+  map.connect ':controller/:action/:ip', :id => nil,
+    :requirements => {:ip => /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/}
+
+  map.connect '/domains/:domain_id/:controller/:id/save_admin_domains', :action => 'save_admin_domains',
+    :requirements => {:domain_id => /\d+/}
+
+  # Install the default route as the lowest priority.
+  map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action/:id'
 end
